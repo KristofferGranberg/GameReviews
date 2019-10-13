c2231728d01d638b1c111eaf71da3201d00a5b26
KristofferGranberg <kristoffergranberg@hotmail.com>
2019-10-14 00:27:16 +0200

    altered front page of app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,114 @@
-import React from 'react';
-import './App.css';
+import React, {Component} from 'react'
+import {Container, Row, Col} from 'reactstrap'
+import ModalForm from './Component/Modals/Modal'
+import GameTable from './Component/Tables/GameTable'
+import {CSVLink} from 'react-csv' // ?
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  const port = 3009
+
+  state = {
+    items: []
+  }
+
+  getItems(){
+    fetch(`http://localhost:${port}/games`)
+      .then( response => response.json())
+      .then(items => this.setState({items}))
+      .catch(err => console.log(err))
+  }
+
+  addItemToState = (item) => {
+    this.setState(prevState => ({
+      items: [...prevState.items, item]
+    }))
+  }
+
+  updateState = (item) =>{
+    const itemIndex = this.state.items.findIndex(data => data.id === item.id)
+    const newArray = [
+      //destructure all items from beginning to the indexed item
+      ...this.state.items.slice(0, itemIndex),
+      //add the updated item to the array
+      item,
+      //add the rest of the items to the array from the index after the replaced item
+      ...this.state.items.slice(itemIndex + 1)
+    ]
+    this.setState({items: newArray})
+  }
+
+  deleteItemFromState = (id) => {
+    const updatedItems = this.state.items.filter(item => item.id !== id)
+    this.setState({items: updatedItems})
+  }
+
+  componentDidMount(){
+    this.getItems()
+  }
+
+  render(){
+    return(
+      <Container className = "App">
+        <Row>
+          <Col>
+          <h1 style = {{margin: "20px 0"}}> GameReviews </h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <GameTable
+              items = {this.state.items}
+              updateState = {this.updateState}
+              deleteItemFromState = {this.deleteItemFromState}/>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <CSVLink
+              filename = {"db.csv"}
+              color = "primary"
+              style = {{float: "left", marginRight: "10px"}}
+              className = "btn btn-primary"
+              data = {this.state.items}>
+            CSV Download
+            </CSVLink>
+            <ModalForm
+              buttonLabel = "Add Game"
+              addItemToState = {this.addItemToState}
+          </Col>
+        </Row>
+      </Container>
+    )
+  }
 }
 
-export default App;
+export default App
+
+
+
+// import React from 'react';
+// import './App.css';
+//
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+//
+// export default App;
